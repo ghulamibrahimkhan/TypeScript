09cc5af8c7ecaf852f06f4c3533468fd573a0df4
G_ibrahim_k <gulamibrahimkhan@gmail.com>
2022-07-05 14:52:16 +0500

    arrays,tuples,enum,function

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,11 @@
 "use strict";
-let age = 20;
-if (age < 30) {
-    age += 20;
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear < 2022) {
+        return income * 1.2;
+    }
+    return income * 1.3;
 }
-console.log(age);
+let xyz;
+xyz = calculateTax(10000);
+console.log(xyz);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,41 @@
 // console.log('Hello World');
 // In terminal we complie our ts file in js by > tsc .\file.ts
 
-let age: number = 20;
-if(age<30){
-    age += 20
-}
-console.log(age);
+// let age: number = 20;
+// if(age<30){
+//     age += 20
+// }
+// console.log(age);
+
+
+// let sales = 123_456_789
+// let course = "typescript"
+// let stats = true;
+// let level;
+
+
+// arrays
+// let numbers : number[] = [1,2,3,4,5,6,7]
+
+// tuples
+// let employee : [number, string] = [1,"Ibrahim"]
+
+//Enum
+
+// enum employee  {"ibrahim" = 1, "Sulaman", Tahira } 
+
+// enum alpa { a,b,c,d,e,f}
+
+// enum cast {Ibrahim = "Khan", Shazaib = "Rao" }
+
+
+// Functions
+// function calculateTax(income: number, taxYear = 2022): number {
+//     if (taxYear < 2022) {
+//         return income * 1.2;
+//     }
+//     return income * 1.3
+// }
+// let xyz :number;
+// xyz = calculateTax(10000)
+// console.log(xyz);
